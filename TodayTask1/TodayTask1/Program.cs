3e4a5a0e1d8abef654ee5736a11863124064594d
BonSeol <hjihyun258@gmail.com>
2025-02-21 17:35:50 +0900

    Task

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Console.WriteLine("Loading.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Console.WriteLine("Loading..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Console.WriteLine("Loading...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Console.WriteLine("Loading.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Console.WriteLine("Loading..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Console.WriteLine("Loading...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Console.WriteLine("Loading.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Console.WriteLine("Loading..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Console.WriteLine("Loading...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Console.WriteLine("Loading.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.WriteLine("Loading..");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("텍스트RPG");
+            Console.WriteLine("Start to Enter");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("당신은 이 세계를 구원하기 위해 소환된 용사.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("당신이 나아갈 길을 선택해주세요");
+            Console.WriteLine("1.전사 2.마법사 3.도적 4.궁수 5.사제");
+            string Class = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine($"{Class}로 선택하시겠나요?");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("이 세계에 뿌리내린 악을 처치하기 위해");
+            Console.WriteLine("6개로 쪼개어진 유물을 찾아야합니다");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("행운을 빕니다, 용사님");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("□□□□□□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Console.WriteLine("세계로 입장하는 중..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("어서오세요, 용사님");
+
+        }
+    }
+}
