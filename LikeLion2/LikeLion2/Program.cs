2edab8086caf4d3612a9e53070e97163d4765415
BonSeol <hjihyun258@gmail.com>
2025-02-21 10:22:56 +0900

    study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age;
+            age = 22;
+
+            Console.WriteLine(age);
+        }
+    }
+}
